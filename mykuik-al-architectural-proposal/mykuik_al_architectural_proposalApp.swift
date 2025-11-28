4df9b91e1b68e8c420732f5555f982a5cc490c4e
Erison Veshi <erison.veshi@raiffeisen.al>
2025-11-28 03:06:50 +0100

    Initial Commit

---
+++ mykuik-al-architectural-proposal/mykuik_al_architectural_proposalApp.swift
@@ -0,0 +1,17 @@
+//
+//  mykuik_al_architectural_proposalApp.swift
+//  mykuik-al-architectural-proposal
+//
+//  Created by Erison Veshi on 28.11.25.
+//
+
+import SwiftUI
+
+@main
+struct mykuik_al_architectural_proposalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mykuik-al-architectural-proposalTests/mykuik_al_architectural_proposalTests.swift
@@ -0,0 +1,17 @@
+//
+//  mykuik_al_architectural_proposalTests.swift
+//  mykuik-al-architectural-proposalTests
+//
+//  Created by Erison Veshi on 28.11.25.
+//
+
+import Testing
+@testable import mykuik_al_architectural_proposal
+
+struct mykuik_al_architectural_proposalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
